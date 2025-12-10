b9866083d43e1f754ba85e6d5d39ae0bca015fd3
Shadra <irfanshadra7@gmail.com>
2025-12-10 23:16:36 +0700

    fixing api

--- src/supabase/supabase.module.ts
+++ src/supabase/supabase.module.ts
@@ -4,8 +4,8 @@ import * as dotenv from 'dotenv';
 
 dotenv.config();
 
-const SUPABASE_URL = process.env.SUPABASE_URL || 'https://zpyzyzdevauthtyoxlnu.supabase.co';
-const SUPABASE_ANON_KEY = process.env.SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpweXp5emRldmF1dGh0eW94bG51Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc2NDU4OTY2NywiZXhwIjoyMDgwMTY1NjY3fQ.wWzYpPAP51ys3QSuqSDrtChLl5_D69waUfkOMed5Yeo';
+const SUPABASE_URL = process.env.SUPABASE_URL!
+const SUPABASE_ANON_KEY = process.env.SUPABASE_ANON_KEY!
 
 const supabase = createClient(
   SUPABASE_URL,
